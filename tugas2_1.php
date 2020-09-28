8dcf10fac073ae8011ccdbcfef3d8155ed80552b
Adi <adiandroo@yahoo.com>
2020-09-28 20:52:56 +0700

    Tugas 2 PHP
    Tugas 2 PHP saya kumpulkan untuk Niomic

---
+++ tugas2_1.php
@@ -0,0 +1,12 @@
+<?php
+echo "36 Hari = 12 Pekerja<br>";
+echo "X hari = 24 Pekerja<br>";
+echo "X = ...?<br>";
+echo "Jawaban : ";
+$hari1 = 36;
+$pekerja1 = 12;
+$pekerja2 = 24;
+$x = $pekerja2 / $pekerja1;
+$y = $hari1 / $x;
+echo "$y hari";
+?>
---
+++ tugas2_2.php
@@ -0,0 +1,10 @@
+<?php
+echo "Jual = 80000<br>";
+echo "Beli = 50000<br>";
+echo "Keuntungan = ?<br>";
+echo "Jawaban : ";
+$hargaAwal = 50000;
+$hargaAkhir = 80000;
+$keuntungan = $hargaAkhir - $hargaAwal;
+echo "$keuntungan";
+?>
